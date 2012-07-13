d6fac023e5bb827334590b18713cc652764f55b8
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-13 13:33:52 +0200

    addressmodel

--- shop_example/test_utils/cli.py
+++ shop_example/test_utils/cli.py
@@ -62,6 +62,7 @@ def configure(**extra):
             'django.contrib.sites',
             'django.contrib.staticfiles',
             'shop',
+            'shop.adressmodel',
             'shop_categories',
             'mptt',
             'treeadmin',
