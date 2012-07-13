5d2a31238e7439b30936161e17502e9e192f9792
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-13 13:12:31 +0200

    at root

--- shop_example/urls.py
+++ shop_example/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    (r'^shop/catalog/', include('shop_categories.urls')),
-    (r'^shop/', include('shop.urls')),
+    (r'^catalog/', include('shop_categories.urls')),
+    (r'^', include('shop.urls')),
 )
