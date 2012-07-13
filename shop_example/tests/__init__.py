486eb6ea9b2c00e8f3a6d5c75ec6076b790230d3
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-13 14:10:06 +0200

    also make testing easy

---
+++ shop_example/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from shop.models.productmodel import Product
+from django.test.testcases import TestCase
+
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        pass
