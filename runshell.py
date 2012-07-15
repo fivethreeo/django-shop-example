1e9460da985f1b731cb5c60acf46105c609cf260
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-15 22:32:49 +0200

    new setup

--- runshell.py
+++ runshell.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python
-from shop_example.test_utils.cli import configure
-from djeasytests.tmpdir import temp_dir
+from djeasytests.testsetup import TestSetup, default_settings
 
-def main():
-    with temp_dir(prefix='shop-example') as STATIC_ROOT:
-        with temp_dir(prefix='shop-example') as MEDIA_ROOT:
-            configure(
-                ROOT_URLCONF='shop_example.urls',
-                STATIC_ROOT=STATIC_ROOT,
-                MEDIA_ROOT=MEDIA_ROOT,
-                DATABASES = {
-                    'default': {
-                        'ENGINE': 'django.db.backends.sqlite3',
-                        'NAME': 'shopexampledb.sqlite',
-                    }
-                }
-            )
-            from django.core.management import call_command
-            call_command('shell')
+default_settings.update(dict(
+    ROOT_URLCONF='shop_example.urls',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'shopexampledb.sqlite',
+        }
+    },
+    INSTALLED_APPS = [
+        'shop_example',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.admin',
+        'django.contrib.sites',
+        'django.contrib.staticfiles', 
+        'shop',
+        'shop_categories',
+        'mptt',
+        'treeadmin',
+        'south',
+        'tinymce'
+    ],
+    SHOP_PRODUCT_MODEL = 'shop_example.models.product.Product',
+    SHOP_ADDRESS_MODEL = 'shop_example.models.address.Address',
+    SHOP_CATEGORIES_CATEGORY_MODEL = 'shop_example.models.category.Category',
+    SHOP_PAYMENT_BACKENDS = [
+        'shop.payment.backends.pay_on_delivery.PayOnDeliveryBackend'
+    ],
+    SHOP_SHIPPING_BACKENDS = [
+        'shop.shipping.backends.flat_rate.FlatRateShipping'
+    ]  
+))
+
+testsetup = TestSetup(appname='djnetaxept', default_settings=default_settings)
 
 if __name__ == '__main__':
-    main()
+    testsetup.run('shell')
--- runtests.py
+++ runtests.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-from djeasytests.runtests import runtests_parse
-from shop_example.test_utils.cli import configure
-    
-if __name__ == '__main__':    
-    runtests_parse(configure=configure,
-        test_labels_prefix='shop_example',
-        default_test_labels=['shop_example'],
-        tmp_dir_prefix='django-shop-example',
-        ROOT_URLCONF='shop_example.urls')
+from runshell import testsetup
+
+if __name__ == '__main__':
+    testsetup.run('tests')
